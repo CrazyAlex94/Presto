36e5819d479f63389a8801c2937bfa234da92050
Alessandro Salerno <alessandrosalerno1994@gmail.com>
2023-02-08 20:37:45 +0100

    modificato login e register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,29 @@
 <x-layout>
     <div class="container">
-        <div class="row mt-5">
+        <div class="row justify-content-center mt-5">
             <div class="col-12 col-md-8 col-lg-7">
                 <h1 class="text-center">Registrati!</h1>
-                <form action="{{route('register')}}" method="POST">
+                <form  method="POST" action="{{route('register')}}">
                     @csrf
                     <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
-                        <input name="name" type="text" class="form-control" id="name" aria-describedby="name">
+                        <label class="form-label">Indirizzo email</label>
+                        <input type="email" class="form-control" name="email">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Indirizzo email</label>
-                        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">Non condividere la tua email con nessuno</div>
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control" name="name">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <div class="d-flex">
-                            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
-                            <input type="checkbox" onclick="myFunction()">
-                        </div>
-                        <script>
-                            function myFunction() {
-                              var x = document.getElementById("exampleInputPassword1");
-                              if (x.type === "password") {
-                                x.type = "text";
-                              } else {
-                                x.type = "password";
-                              }
-                            }
-                        </script>
+                        <label  class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password">
                     </div>
                     <div class="mb-3">
-                        <label for="password_confirmation" class="form-label">Conferma Password</label>
-                        <input name="password" type="password" class="form-control" id="password_confirmation">
+                        <label  class="form-label">Conferma password</label>
+                        <input type="password" class="form-control" name="password_confirmation">
                     </div>
                     <button type="submit" class="btn btn-primary mb-3">Registrati</button>
-                    <div>Sei già registrato?<a href="{{route('login')}}">Clicca qui!</a></div>
                 </form>
+                <div>Sei già registrato?<a href="{{route('login')}}">Clicca qui!</a></div>
             </div>
         </div>
     </div>
